182ef638620304cb4e6e9297456828bc21579829
Fouzan Karim <fouzankarim@MacBook-Air.local>
2020-04-17 20:10:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,350 @@
+/* eslint-disable no-undef */
+//Tracking Information Function Call
+$(document).ready(function() {
+  $('.orderTkNum').click(function() {
+    let num = $('#orderNum').val();
+    if (num === '') {
+      // eslint-disable-next-line no-alert
+      alert('Enter Tracking Number');
+      return;
+    }
+    //Call 17track method
+    YQV5.trackSingle({
+      YQ_ContainerId: 'pkTk',
+      YQ_Height: 560,
+      YQ_Fc: '0',
+      YQ_Lang: 'en',
+      YQ_Num: num
+    });
+  });
+});
+
+//Hover on thumbnails changes images
+$(document).ready(function() {
+  $('#i1').hover(function() {
+    $('#full').attr('src', $(this).attr('src'));
+  });
+  $('#i2').hover(function() {
+    $('#full').attr('src', $(this).attr('src'));
+  });
+  $('#i3').hover(function() {
+    $('#full').attr('src', $(this).attr('src'));
+  });
+  $('#i4').hover(function() {
+    $('#full').attr('src', $(this).attr('src'));
+  });
+  $('#ap1').hover(function() {
+    $('#full5').attr('src', $(this).attr('src'));
+  });
+  $('#ap2').hover(function() {
+    $('#full5').attr('src', $(this).attr('src'));
+  });
+  $('#ap3').hover(function() {
+    $('#full5').attr('src', $(this).attr('src'));
+  });
+
+  $('#gf1').hover(function() {
+    $('#full4').attr('src', $(this).attr('src'));
+  });
+  $('#gf2').hover(function() {
+    $('#full4').attr('src', $(this).attr('src'));
+  });
+  $('#gf3').hover(function() {
+    $('#full4').attr('src', $(this).attr('src'));
+  });
+  $('#gf3').hover(function() {
+    $('#full4').attr('src', $(this).attr('src'));
+  });
+  $('#oc1').hover(function() {
+    $('#full1').attr('src', $(this).attr('src'));
+  });
+  $('#oc2').hover(function() {
+    $('#full1').attr('src', $(this).attr('src'));
+  });
+  $('#oc3').hover(function() {
+    $('#full1').attr('src', $(this).attr('src'));
+  });
+  $('#x1').hover(function() {
+    $('#fullx').attr('src', $(this).attr('src'));
+  });
+  $('#x2').hover(function() {
+    $('#fullx').attr('src', $(this).attr('src'));
+  });
+  $('#x3').hover(function() {
+    $('#fullx').attr('src', $(this).attr('src'));
+  });
+  $('#x4').hover(function() {
+    $('#fullx').attr('src', $(this).attr('src'));
+  });
+  $('#s1').hover(function() {
+    $('#fulls').attr('src', $(this).attr('src'));
+  });
+  $('#s2').hover(function() {
+    $('#fulls').attr('src', $(this).attr('src'));
+  });
+  $('#s3').hover(function() {
+    $('#fulls').attr('src', $(this).attr('src'));
+  });
+  $('#mb1').hover(function() {
+    $('#full3').attr('src', $(this).attr('src'));
+  });
+  $('#mb2').hover(function() {
+    $('#full3').attr('src', $(this).attr('src'));
+  });
+  $('#mb3').hover(function() {
+    $('#full3').attr('src', $(this).attr('src'));
+  });
+  $('#bt1').hover(function() {
+    $('#full2').attr('src', $(this).attr('src'));
+  });
+  $('#bt2').hover(function() {
+    $('#full2').attr('src', $(this).attr('src'));
+  });
+  $('#bt3').hover(function() {
+    $('#full2').attr('src', $(this).attr('src'));
+  });
+  $('#rk1').hover(function() {
+    $('#full6').attr('src', $(this).attr('src'));
+  });
+  $('#rk2').hover(function() {
+    $('#full6').attr('src', $(this).attr('src'));
+  });
+  $('#rk3').hover(function() {
+    $('#full6').attr('src', $(this).attr('src'));
+  });
+  $('#so1').hover(function() {
+    $('#full7').attr('src', $(this).attr('src'));
+  });
+  $('#so2').hover(function() {
+    $('#full7').attr('src', $(this).attr('src'));
+  });
+  $('#ps1').hover(function() {
+    $('#full9').attr('src', $(this).attr('src'));
+  });
+  $('#ps2').hover(function() {
+    $('#full9').attr('src', $(this).attr('src'));
+  });
+  $('#ps3').hover(function() {
+    $('#full9').attr('src', $(this).attr('src'));
+  });
+  $('#sw1').hover(function() {
+    $('#full8').attr('src', $(this).attr('src'));
+  });
+  $('#sw2').hover(function() {
+    $('#full8').attr('src', $(this).attr('src'));
+  });
+  $('#sw3').hover(function() {
+    $('#full8').attr('src', $(this).attr('src'));
+  });
+});
+
+$(document).ready(function() {
+  $('#orderNo').click(function() {
+    $('.orderNoDiv').css('display', 'block');
+    $('#radTxt').prop('required', true);
+  });
+  $('#cmnt').click(function() {
+    $('#radTxt').prop('required', false);
+    $('.orderNoDiv').css('display', 'none');
+  });
+  $('#qst').click(function() {
+    $('#radTxt').prop('required', false);
+    $('.orderNoDiv').css('display', 'none');
+  });
+});
+
+function openNav() {
+  $('.dropdwncontainer').removeClass('drpdwntggl');
+  $('#sidebarNav').attr('style', 'width: 300px !important');
+}
+
+function closeNav() {
+  $('#sidebarNav').attr('style', 'width: 0 !important');
+  $('.dropdwncontainer').removeClass('drpdwntggl');
+}
+$(document).ready(function() {
+  $('.drpdwn').click(function() {
+    $('.dropdwncontainer').toggleClass('drpdwntggl');
+  });
+});
+
+$(document).ready(function() {
+  if (window.location.hash === '#hp') {
+    $('.hp').removeClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+  } else if (window.location.hash === '#phns') {
+    $('.hp').addClass('hidden');
+    $('.phns').removeClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+  } else if (window.location.hash === '#hm') {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').removeClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+  } else if (window.location.hash === '#lptp') {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').removeClass('hidden');
+    $('.vgm').addClass('hidden');
+  } else if (window.location.hash === '#vdg') {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').removeClass('hidden');
+  }
+
+  $('#phone').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').removeClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+  });
+
+  $('#home').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').removeClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+  });
+
+  $('#laptop').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').removeClass('hidden');
+    $('.vgm').addClass('hidden');
+  });
+
+  $('#vdg').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').removeClass('hidden');
+  });
+
+  $('#headphones').click(function() {
+    $('.hp').removeClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+  });
+
+  $('#ps').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').removeClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+    closeNav();
+  });
+
+  $('#hms').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').removeClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+    closeNav();
+  });
+
+  $('#lptps').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').removeClass('hidden');
+    $('.vgm').addClass('hidden');
+    closeNav();
+  });
+
+  $('#vgms').click(function() {
+    $('.hp').addClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').removeClass('hidden');
+    closeNav();
+  });
+  $('#hps').click(function() {
+    $('.hp').removeClass('hidden');
+    $('.phns').addClass('hidden');
+    $('.hm').addClass('hidden');
+    $('.lptp').addClass('hidden');
+    $('.vgm').addClass('hidden');
+    closeNav();
+  });
+});
+
+
+  //Client Side Form Validation
+  
+  let validateJS= function(){
+  let fn=document.getElementById('firstName').value.trim();
+  let ln=document.getElementById('lastName').value.trim();
+  let full=fn+ln;
+  full.toUpperCase();
+  let alpha=true;
+  if(full.length>4){
+  for(i of full){
+    if (full[i]<"A" || full[i]>"Z"){
+      alpha=false;
+    }
+  }
+  }
+else{
+  alpha=false;
+}
+
+
+let email=false;
+let mailtest=/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$/;
+let ef = document.getElementById('email').value;
+if(ef.match(mailtest)){
+  email=true; 
+}
+
+
+let adrs= false;
+let adtest=/^\d+\s[A-za-z]+\s[A-Za-z]+[.]?(\s[A-Za-z]+[.]?)*/;
+let af= document.getElementById('address').value;
+if(af.match(adtest)){
+  adrs=true; 
+}
+
+let zip= false;
+let ziptest= /^[A-Za-z][0-9][A-Za-z]\s?[0-9][A-Za-z][0-9]$/;
+let zpf = document.getElementById('PostalCode').value;
+if(zpf.match(ziptest)){
+  zip = true;
+}
+
+
+let pn= false;
+let pntest= /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$/;
+let pnf = document.getElementById('phoneNum').value;
+if(pnf.match(pntest)){
+  pn = true;
+}
+
+
+let txt= true;
+let txtF= document.getElementById("message").value;
+if(txtF.trim().length===0){
+  txt=false;
+}
+
+
+return (txt && pn && zip && email && alpha && adrs);
+};
+
